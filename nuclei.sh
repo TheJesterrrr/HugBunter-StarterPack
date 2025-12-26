d7c2d039f188437461ed1a6ae2ec5a6bb6b02f2e
Divo <danielsaputradivo@gmail.com>
2025-12-27 06:17:43 +0700

    Change: melakukan perubahan terhadap struktur kode

--- nuclei.sh
+++ nuclei.sh
@@ -1,10 +1,25 @@
 #!/bin/bash 
 
+# Melakukan check package dan upgrade
 sudo apt-get update && sudo apt-get upgrade -y
 
+# menginstall Golang
 sudo apt-get install -y golang
 
-go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+# konfirmasi pemasangan Nuclei
+read -p "Lanjut memasang Nuclei? (Y/N): " response
+response_lower=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response_lower" == "yes" || "$response_lower" == "y" ]]; then
+    echo "Menginstal package..."
+    go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+    echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc && source ~/.bashrc
+    echo "Package berhasil diinstal!"
+elif [[ "$response_lower" == "no" || "$response_lower" == "n" ]]; then
+    echo "Instalasi dibatalkan"
+    exit 1
+else
+    echo "Input tidak valid!"
+    exit 1
+    fi
 
-echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.bashrc && source ~/.bashrc
 
